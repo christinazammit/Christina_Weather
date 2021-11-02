678ba0511f7d7b8b7dee9abc53808d9ce979804c
Christina Zammit <zammichr@sheridancollege.ca>
2021-11-01 20:57:13 -0400

    Initial Commit

---
+++ Christina_Weather/Christina_WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  Christina_WeatherApp.swift
+//  Christina_Weather
+//
+//  Created by Christina Zammit on 2021-11-01.
+//
+
+import SwiftUI
+
+@main
+struct Christina_WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Christina_Weather/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Christina_Weather
+//
+//  Created by Christina Zammit on 2021-11-01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
